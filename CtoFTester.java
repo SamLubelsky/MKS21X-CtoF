fc05401535caa2b67757f5f0b95c6487ef403ef9
John Doe <slubelsky30@stuy.edu>
2021-09-25 12:43:24 -0400

    celsius to fahrenheit tests added

--- CtoFTester.java
+++ CtoFTester.java
@@ -6,6 +6,11 @@ class CtoFTester{
     return (fahrenheit - 32) / 1.8;
   }
   public static void main(String[] args){
+    print(celsiusToFahrenheit(0)); // 32
+    print(celsiusToFahrenheit(10)); //50
+    print(celsiusToFahrenheit(105.5)); //221.9
+    print(celsiusToFahrenheit(-22)); //-7.6
+    print(celsiusToFahrenheit(-2.6)); //27.32
 
   }
 }
