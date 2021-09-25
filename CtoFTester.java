a2690827a54c9805c076296db1835ad6dc862c97
John Doe <slubelsky30@stuy.edu>
2021-09-25 12:38:11 -0400

    created class, main, cToF, and fToC functions

---
+++ CtoFTester.java
@@ -0,0 +1,11 @@
+class CtoFTester{
+  public double celsiusToFahrenheit(double celsius){
+    return (celsius * 1.8) + 32;
+  }
+  public double fahrenheitToCelsius(double fahrenheit){
+    return (fahrenheit - 32) / 1.8;
+  }
+  public static void main(String[] args){
+
+  }
+}
