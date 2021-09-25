2cd8ed4cf50a2b0fa62d2c099aed8db5ad9993dd
John Doe <slubelsky30@stuy.edu>
2021-09-25 12:51:48 -0400

    bug fixes, all tests passed

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,21 +1,21 @@
 class CtoFTester{
-  public double celsiusToFahrenheit(double celsius){
+  public static double celsiusToFahrenheit(double celsius){
     return (celsius * 1.8) + 32;
   }
-  public double fahrenheitToCelsius(double fahrenheit){
+  public static double fahrenheitToCelsius(double fahrenheit){
     return (fahrenheit - 32) / 1.8;
   }
   public static void main(String[] args){
-    print(celsiusToFahrenheit(0)); // 32
-    print(celsiusToFahrenheit(10)); //50
-    print(celsiusToFahrenheit(105.5)); //221.9
-    print(celsiusToFahrenheit(-22)); //-7.6
-    print(celsiusToFahrenheit(-2.6)); //27.32
+    System.out.println(celsiusToFahrenheit(0)); // 32
+    System.out.println(celsiusToFahrenheit(10)); //50
+    System.out.println(celsiusToFahrenheit(105.5)); //221.9
+    System.out.println(celsiusToFahrenheit(-22)); //-7.6
+    System.out.println(celsiusToFahrenheit(-2.6)); //27.32
 
-    print(fahrenheitToCelsius(5)); //-15
-    print(fahrenheitToCelsius(32)); //0
-    print(fahrenheitToCelsius(54.2)); //12.3333..
-    print(fahrenheitToCelsius(-1)); //-18.3333..
-    print(fahrenheitToCelsius(-150.6)); //-101.4444..
+    System.out.println(fahrenheitToCelsius(5)); //-15
+    System.out.println(fahrenheitToCelsius(32)); //0
+    System.out.println(fahrenheitToCelsius(54.2)); //12.3333..
+    System.out.println(fahrenheitToCelsius(-1)); //-18.3333..
+    System.out.println(fahrenheitToCelsius(-150.6)); //-101.4444..
   }
 }
